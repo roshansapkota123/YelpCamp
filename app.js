c2af1e56676d8357664c1256733b0e87d14d07f5
Roshan <Roshan.sapkota@selu.edu>
2019-09-22 23:32:50 -0500

    added env port

--- app.js
+++ app.js
@@ -54,8 +54,8 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-// app.listen(process.env.PORT, process.env.IP, function() {
-//     console.log("The YelpCamp Server Has Started!!!");
-// });
+app.listen(process.env.PORT|| 5000, process.env.IP, function() {
+    console.log("The YelpCamp Server Has Started!!!");
+});
 
-app.listen("5000");
+// app.listen("5000");
